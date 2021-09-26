59ba5659370dc4b8f5e7ac576ef9fd5704cf5204
riz-ahmed <rzwn.hmd@gmail.com>
2021-09-26 15:52:48 +0200

    import csv and plot a basic figure

--- import_data_in_csv.py
+++ import_data_in_csv.py
@@ -1,5 +1,21 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # import a csv data
-file_location = ""
-ms = pd.DataFrame.read_csv('file_locaiton')
+file = "FB.csv"
+fb_data = pd.read_csv(file)
+# Ensure the dataframe is loaded properly
+print(fb_data.head())
+# print the size of the dataframe
+print(fb_data.shape)
+# summary of the statistics for this dataframe
+print(fb_data.describe())
+
+# change the date columns from str to date-time so as to plot the data
+fb_data['Date'] = pd.to_datetime(fb_data.Date)
+
+# print the close of the stocks form the imported data
+plt.figure(figsize=(10, 8))
+fb_data.set_index('Date', inplace=True)
+fb_data['Close'].plot()
+plt.show()
